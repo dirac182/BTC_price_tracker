77b9f3750ea5217b3c7ab54f3e644cccadd12f24
Derrick <dmorin9696@yahoo.com>
2023-02-14 00:39:21 -0600

    I made a mess but the send_notif function has all the info and is ready to use twilio. Good luck!

--- main.py
+++ main.py
@@ -22,23 +22,27 @@ NEWS_PARAMS = {
     "q": "bitcoin",
     "apiKey": NEWS_API_KEY,
     "sortBy": "relevancy",
-    "pageSize": 5,
+    "pageSize": 2,
     "from": str(days_ago)
 }
 
-def pull_articles():
+def pull_articles(tkr: str, movement: str, price: float,change: float):
     news_api = requests.get(url=NEWS_ENDPOINT, params=NEWS_PARAMS)
     news_data = news_api.json()["articles"]
     for arts in news_data:
         source = arts["source"]["name"]
         date = arts["publishedAt"].split("T")[0]
         title = arts["title"]
+        description = arts["description"]
         url = arts["url"]
         print("Source: " + source)
         print("Date: " + date)
         print("Title: " + title)
+        print("Description: " + description)
         print("URL: " + url)
-
+        send_notif(tkr=tkr,movem=movement,title=title,date=date,descr=description,url=url, change=change,price=price)
+def send_notif(tkr: str, movem: str,title: str, date: str, descr: str, url: str, change: float, price: float):
+    pass
 def update_btc_data(trk: str, date: str, time: str, price: float, move: str, change: float):
     # with open(file="btc_data.csv", mode="w") as file:
     df = pandas.read_csv("btc_data.csv")
@@ -52,6 +56,8 @@ def update_btc_data(trk: str, date: str, time: str, price: float, move: str, cha
     }
     new_data = pandas.DataFrame(data)
     new_data.to_csv("btc_data.csv",mode="a",index=False,header=False)
+    if float(change) >= 1.5:
+        pull_articles(tkr=trk, movement= move, price= price ,change= change)
 
 old_data = pandas.read_csv("btc_data.csv")
 last_row = old_data.tail(1)
@@ -77,14 +83,17 @@ def btc_check():
     elif now_price/before_price == 1:
         move = "No Change"
         print("No Change")
-    percent_change = round(abs((now_price-before_price)/before_price),6)
+    percent_change = round(abs(((now_price-before_price)/((before_price + now_price)/2))*100),6)
     if percent_change > .99999:
         pass
     update_btc_data(trk= ticker,date=date,time=time, price=now_price, move=move, change=percent_change)
     print("Current Price: " + str(now_price))
     print("Last Price: " + str(last_btc_price))
+
+
+
     last_btc_price = now_price
 
 while True:
     btc_check()
-    time.sleep(60*5)
+    time.sleep(30)
